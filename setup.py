4533c95de6eec2f7b8c918e00b23fb0e8567c819
Gajender <Iamsanju0707@gmail.com>
2023-04-03 14:15:49 +0530

    this is commit

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup,mypackages
+from setuptools import setup,find_packages
 
 
 def Requirements():
     with open("requirements.txt","r") as f:
         r=f.readlines()
         r=[ i.replace("\n","") for i in r ]
+        if "-e ." in r:
+            r.remove("-e .")
+            
         return r
 
 
@@ -13,7 +16,6 @@ setup(
     author="Gajender",
     version="0.1",
     description="This is Test Project",
-    packages=mypackages(),
+    packages=find_packages(),
     install_requires=Requirements()
 )
-
