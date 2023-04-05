b7363bc62faea23f977b7623f059aeb25a9eea7a
Gajender <Iamsanju0707@gmail.com>
2023-04-05 15:51:50 +0530

    commited

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -42,6 +42,6 @@ class DataIngestion:
         except Exception as e:
             raise CustomError(e)
         
-if __name__=="__main__":
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()
+# if __name__=="__main__":
+#     obj=DataIngestion()
+#     obj.initiate_data_ingestion()
