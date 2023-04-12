859ad0ba002d7ac7df01a91845d982618d7a6a97
Gajender <Iamsanju0707@gmail.com>
2023-04-12 11:44:43 +0530

    adding the files

---
+++ src/application.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,request
+from predict_pipeline import CustomData,PredictPipeline
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return  render_template("index.html")
+
+@app.route("/predict",methods=["POST","GET"])
+def predict_datapoints():
+    if request.method=="GET":
+        return render_template("index.html")
+    else:
+        data=CustomData(
+            age=request.form.get("age"),
+            sex=request.form.get("sex"),
+            bmi=request.form.get("bmi"),
+            children=request.form.get("children"),
+            smoker=request.form.get("smoker"),
+            region=request.form.get("region")
+  
+        )
+    pref_data=data.get_values_as_dataframe()
+    # print(pref_data)
+
+    predict_pipeline=PredictPipeline()
+    result=predict_pipeline.predict(pref_data)
+    # print(result)
+    return render_template("home.html",result=result[0])
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=True)
